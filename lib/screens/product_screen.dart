5aedb9aa643903560c71bf6701a6208514fcf954
DaviFreitas26 <51301100+DaviFreitas26@users.noreply.github.com>
2020-03-12 09:13:32 -0300

    Update product_screen.dart
    Implements:
    Product sizes bottons;
    Add to cart;
    Description

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -12,6 +12,9 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
+
+  String size;
+
   _ProductScreenState(this.product);
 
   @override
@@ -52,8 +55,64 @@ class _ProductScreenState extends State<ProductScreen> {
                 ),
                 Text(
                   "R\$ ${product.price.toStringAsFixed(2)}",
-                  style: TextStyle(fontSize:22.0, fontWeight: FontWeight.bold, color: primaryColor),
-                )
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ),
+                SizedBox(height: 16.0),
+                Text("Tamanho",
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500)),
+                SizedBox(
+                    height: 34.0,
+                    child: GridView(
+                      padding: EdgeInsets.symmetric(vertical: 4.0),
+                      scrollDirection: Axis.horizontal,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 1,
+                          mainAxisSpacing: 8.0,
+                          childAspectRatio: 0.5),
+                      children: product.sizes.map((s) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              size = s;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0)),
+                                border: Border.all(
+                                    color: s == size
+                                        ? primaryColor
+                                        : Colors.grey[500],
+                                    width: 3.0)),
+                            width: 50.0,
+                            alignment: Alignment.center,
+                            child: Text(s),
+                          ),
+                        );
+                      }).toList(),
+                    )),
+                SizedBox(height: 16.0),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                      onPressed: size != null ? () {} : null,
+                      child: Text(
+                        "Adicionar ao Carrinho",
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      color: primaryColor,
+                      textColor: Colors.white),
+                ),
+                SizedBox(height: 16.0),
+                Text("Descrição",
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500)),
+                        Text(product.description, style: TextStyle(fontSize: 16.0))
               ]),
         )
       ]),
