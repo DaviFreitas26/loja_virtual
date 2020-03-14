48213ca4004c19e74bf2e373df32abee8d85c9a5
DaviFreitas26 <51301100+DaviFreitas26@users.noreply.github.com>
2020-03-14 15:47:22 -0300

    Implements Login Screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:
+            AppBar(title: Text("Entrar"), centerTitle: true, actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'CRIAR CONTA',
+              style: TextStyle(fontSize: 15.0),
+            ),
+            textColor: Colors.white,
+            onPressed: () {},
+          )
+        ]),
+        body: Form(
+          child: ListView(padding: EdgeInsets.all(16.0), children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(hintText: 'E-mail'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Senha'),
+              obscureText: true,
+            ),
+            Align(alignment: Alignment.centerRight,child: FlatButton(
+              onPressed: (){}, child: Text('Esqueci minha senha', textAlign: TextAlign.right), padding: EdgeInsets.zero, )
+              ),
+              SizedBox(height: 16.0),
+              SizedBox(
+                height: 44.0,
+                child: RaisedButton(
+                child: Text('Entrar',  
+                style: TextStyle(
+                  fontSize: 18.0)),
+                  textColor: Colors.white,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: (){},
+                  )
+              )
+          ]),
+        ));
+  }
+}
