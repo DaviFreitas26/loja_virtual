fc96fec3aae3f273962b751b574f61367ba55026
Davi Freitas <51301100+DaviFreitas26@users.noreply.github.com>
2020-03-16 22:17:36 -0300

    New Pages

--- lib/datas/product_data.dart
+++ lib/datas/product_data.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ProductData{
+class ProductData {
   String category;
   String id;
   String title;
@@ -11,12 +11,16 @@ class ProductData{
   List images;
   List sizes;
 
-  ProductData.fromDocuments(DocumentSnapshot snapshot){
+  ProductData.fromDocuments(DocumentSnapshot snapshot) {
     id = snapshot.documentID;
     title = snapshot.data['title'];
     description = snapshot.data['description'];
     price = snapshot.data['price'];
     images = snapshot.data['images'];
     sizes = snapshot.data['sizes'];
+  }
+
+  Map<String, dynamic> toResumedMap() {
+    return {'title': title, 'description': description, 'price': price};
   }
 }
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,9 +1,14 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/datas/cart_product.dart';
 import 'package:loja_virtual/datas/product_data.dart';
+import 'package:loja_virtual/models/cart_model.dart';
+import 'package:loja_virtual/models/user_model.dart';
+import 'package:loja_virtual/screens/login_screen.dart';
 
 class ProductScreen extends StatefulWidget {
   final ProductData product;
+
   ProductScreen(this.product);
 
   @override
@@ -100,9 +105,25 @@ class _ProductScreenState extends State<ProductScreen> {
                 SizedBox(
                   height: 44.0,
                   child: RaisedButton(
-                      onPressed: size != null ? () {} : null,
-                      child: Text(
-                        "Adicionar ao Carrinho",
+                      onPressed: size != null
+                          ? () {
+                              if (UserModel.of(context).isLoggedIn()) {
+
+                                CartProduct cartProduct = CartProduct();
+                                cartProduct.size = size;
+                                cartProduct.quantity = 1;
+                                cartProduct.pid = product.id;
+                                cartProduct.category = product.category;
+
+                                CartModel.of(context).addCartItem(cartProduct);
+                              } else {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => LoginScreen()));
+                              }
+                            }
+                          : null,
+                      child: Text(UserModel.of(context).isLoggedIn() ?
+                        'Adicionar ao Carrinho':'Entre para comprar',
                         style: TextStyle(fontSize: 18.0),
                       ),
                       color: primaryColor,
@@ -112,7 +133,7 @@ class _ProductScreenState extends State<ProductScreen> {
                 Text("Descrição",
                     style:
                         TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500)),
-                        Text(product.description, style: TextStyle(fontSize: 16.0))
+                Text(product.description, style: TextStyle(fontSize: 16.0))
               ]),
         )
       ]),
